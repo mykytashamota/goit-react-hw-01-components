7f5583044de1244509e253a8427dfb4bed45006d
mykytashamota <nik.shamota@gmail.com>
2023-09-02 20:38:40 +0300

    1 task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import user from 'path/to/user.json';
+import ProfileUser from './profile/profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ProfileUser
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
   );
 };
---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,42 @@
+import css from 'components/profile/profile.module.css';
+// import PropTypes from 'prop-types';
+
+const ProfileUser = ({
+  username,
+  tag,
+  location,
+  avatar,
+  followers,
+  views,
+  likes,
+}) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProfileUser;
+
+// ReactDOM.render(<App />, document.querySelector('#root'));
